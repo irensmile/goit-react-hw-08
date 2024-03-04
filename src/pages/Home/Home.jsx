421c7c7b20018cf484aeaa840ace813dcc1abcb5
irensmile <curkan.ira@gmail.com>
2024-03-03 21:37:28 -0500

    typo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
 import DocumentTitle from "../../components/DocumentTitle";
 
 export const Home = () => (
-    <>
-         <DocumentTitle>Home Page</DocumentTitle>
-        <div>Come back later</div>
-        <div>Some exciding info will be posted here soon</div>
-    </>
-)
+  <>
+    <DocumentTitle>Home Page</DocumentTitle>
+    <div>Come back later</div>
+    <div>Some exciting info will be posted here soon</div>
+  </>
+);
 export default Home;
