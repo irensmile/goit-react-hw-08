0cda72c078eb6387c566f4177c765591ff46d585
irensmile <curkan.ira@gmail.com>
2024-03-03 21:57:04 -0500

    clean up form after the operation is done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,45 +1,46 @@
-
 import { useDispatch } from "react-redux";
-import {login} from '../../redux/auth/operations'
-import css from './Login.module.css'
-;
+import { login } from "../../redux/auth/operations";
+import css from "./Login.module.css";
 import DocumentTitle from "../../components/DocumentTitle";
 import toast from "react-hot-toast";
 
 export const Login = () => {
-    console.log('Rendering loging');
-    const dispatch = useDispatch();
+  console.log("Rendering loging");
+  const dispatch = useDispatch();
 
-
-    const handleSubmit = e => {
-        e.preventDefault()
-        const form = e.target;
-        dispatch(login({
-            email: form.elements.email.value,
-            password: form.elements.password.value
-        }))
-        .unwrap()
-        .catch(()=>{
-            toast(`Error on login`);
-        })
-        e.target.reset();
-    }
-    return (
-        <>
-            <DocumentTitle>Login</DocumentTitle>
-            <form className={css.form} onSubmit={handleSubmit}>
-                <label className={css.element}>
-                    Email
-                    <input name="email"></input>
-                </label>
-                <label className={css.element}>
-                    Password
-                    <input  name="password" type="password"></input>
-                </label>    
-                <button type="submit">Login</button>
-        </form>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    dispatch(
+      login({
+        email: form.elements.email.value,
+        password: form.elements.password.value,
+      })
+    )
+      .unwrap()
+      .then(() => {
+        form.reset();
+      })
+      .catch(() => {
+        toast(`Error on login`);
+      });
+  };
+  return (
+    <>
+      <DocumentTitle>Login</DocumentTitle>
+      <form className={css.form} onSubmit={handleSubmit}>
+        <label className={css.element}>
+          Email
+          <input name="email"></input>
+        </label>
+        <label className={css.element}>
+          Password
+          <input name="password" type="password"></input>
+        </label>
+        <button type="submit">Login</button>
+      </form>
     </>
-
-)}
+  );
+};
 
 export default Login;
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,45 +1,48 @@
 import { useDispatch } from "react-redux";
- import {signup} from '../../redux/auth/operations'
- import css from './Register.module.css'
+import { signup } from "../../redux/auth/operations";
+import css from "./Register.module.css";
 import DocumentTitle from "../../components/DocumentTitle";
 
 export const Register = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-
-    const handleSubmit = e => {
-        e.preventDefault()
-        const form = e.target;
-        dispatch(signup({
-            name: form.elements.name.value,
-            email: form.elements.email.value,
-            password: form.elements.password.value
-        }))
-        .unwrap()
-        .catch(()=>{
-            toast(`Error on register`);
-        })
-        e.target.reset();
-    }
-    return (
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    dispatch(
+      signup({
+        name: form.elements.name.value,
+        email: form.elements.email.value,
+        password: form.elements.password.value,
+      })
+    )
+      .unwrap()
+      .then(() => {
+        form.reset();
+      })
+      .catch(() => {
+        toast(`Error on register`);
+      });
+  };
+  return (
     <>
-        <DocumentTitle>Register</DocumentTitle>
-        <form className={css.form} onSubmit={handleSubmit}>
+      <DocumentTitle>Register</DocumentTitle>
+      <form className={css.form} onSubmit={handleSubmit}>
         <label className={css.element}>
-            Name
-            <input name="name"></input>
+          Name
+          <input name="name"></input>
         </label>
         <label className={css.element}>
-            Email
-            <input name="email"></input>
+          Email
+          <input name="email"></input>
         </label>
         <label className={css.element}>
-            Password
-            <input  name="password" type="password"></input>
-        </label>    
+          Password
+          <input name="password" type="password"></input>
+        </label>
         <button type="submit">Register</button>
-    </form>
+      </form>
     </>
-    
-)}
+  );
+};
 export default Register;
